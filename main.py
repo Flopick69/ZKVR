a2e92d95a25195994168331fcb5072a4ba5307aa
Flopick69 <zakirovartem09@gmail.com>
2026-07-01 22:33:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from aiogram.filters import Command, CommandStart
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 # Токен твоего бота и ID администратора (замени на свои актуальные данные)
-BOT_TOKEN = "ТВОЙ_ТОКЕН_БОТА"
-MY_ID = 123456789  # Замени на свой реальный Telegram ID
+BOT_TOKEN = "8940239980:AAH1u8qqQo9MtSpv4KHLlRcr6ckm3s3_ZQI"
+MY_ID = 8344626747  # Замени на свой реальный Telegram ID
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
